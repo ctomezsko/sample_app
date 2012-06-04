30bde08010faf79b10f675968f2df94ace7a69b6
Catherine Leary Tomezsko <ctomezsko@gmail.com>
2012-06-04 11:17:28 -0400

    Fixed titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,8 +30,13 @@ end
     it "should have the base title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-            :text => "Ruby on Rails Tutorial Help")
+            :text => "Ruby on Rails Tutorial Sample App")
   
+  end
+  
+   it "should not have a custom page title" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title', :text => '| Help')
   end
   
 end
@@ -46,8 +51,13 @@ end
     it "should have the base title" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-              :text => "#{base_title} | About Us")
+              :text => "Ruby on Rails Tutorial Sample App")
     
+  end
+  
+   it "should not have a custom page title" do
+      visit '/static_pages/about'
+      page.should_not have_selector('title', :text => '| About')
   end
 end
   
@@ -61,7 +71,14 @@ end
   it "should have the title 'Contact'" do
     visit '/static_pages/contact'
     page.should have_selector('title',
-                :text => "#{base_title} | Contact" )
+                :text => "Ruby on Rails Tutorial Sample App" )
   end
+  
+   it "should not have a custom page title" do
+      visit '/static_pages/contact'
+      page.should_not have_selector('title', :text => '| Contact')
+ 
+  end
+  
 end
 end

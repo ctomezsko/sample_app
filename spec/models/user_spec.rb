f751c5da553837561e58ac1c914b4406bb1aec18
Catherine Leary Tomezsko <ctomezsko@gmail.com>
2012-06-15 19:48:40 -0400

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,8 +29,14 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
   
-  it { should be_valid }
+  # it { should be_valid } -- repeats so think unnessecary
   
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
@@ -43,6 +49,62 @@ describe User do
     
     it { should be_admin }
   end
+  
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+  
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid}
+  end
+  
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                       foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end      
+    end
+  end    # me thinks this might be unnessecary
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end      
+    end
+  end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+  
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end  
 
   
   describe "when password is not present" do
@@ -80,57 +142,11 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
-
   
     describe "remember token" do
      before { @user.save }
      its(:remember_token) { should_not be_blank }
    end
-    
-  describe "when name is not present" do
-    before { @user.name = " " }
-    it { should_not be_valid }
-  end
-  
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
-  
-  describe "when email is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid}
-  end
-  
-  describe "when email format is invalid" do
-    it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                       foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
-      end      
-    end
-  end
-
-  describe "when email format is valid" do
-    it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        @user.should be_valid
-      end      
-    end
-  end
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.email = @user.email.upcase
-      user_with_same_email.save
-    end
-
-    it { should_not be_valid }
-  end
 
   describe "micropost associations" do
 
@@ -158,12 +174,45 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+      
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
   
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+    
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+    
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user)}
+    end
+    
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+      
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end 
 end
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,8 +4,6 @@ describe "Authentication" do
   
   subject { page }
   
-  
-  
   describe "signin page" do
     before { visit signin_path }
     
@@ -75,26 +73,27 @@ describe "Authentication" do
             fill_in "Password", with: user.password
             click_button "Sign in"
           end
+         # removed end and put on line 84
+          
+          describe "in the Relationships controller" do
+            describe "submitting to the create action" do
+              before { post relationships_path }
+              specify { response.should redirect_to(signin_path) }
+            end
+            
+            describe "submitting to the destroy action" do
+              before { delete relationship_path(1) }
+              specify { response.should redirect_to(signin_path) }
+            end
+          end
           
           describe "after signing in" do
             
             it "should render the desired protected page" do
               page.should have_selector('title', text: 'Edit user')
             end
-          end 
-
-        
-        describe "as non-admin user" do
-          let(:user) { FactoryGirl.create(:user) }
-          let(:non_admin) { FactoryGirl.create(:user) }
-          
-          before { sign_in non_admin }
-          
-          describe "submitting a DELETE request to the Users#destroy action" do
-            before { delete user_path(user) }
-            specify { response.should redirect_to(root_path) }
-          end
-        end
+          end # just added
+        end # took from line 159
         
         describe "in the Microposts controller" do
 
@@ -108,42 +107,71 @@ describe "Authentication" do
             specify { response.should redirect_to(signin_path) }
           end
         end
-      end
-      
-      describe "in the Users controller" do
-          
-          describe "visiting the user index" do
-            before { visit users_path }
+        # removed end and put on line 160
+        
+        describe "in the Users controller" do
+
+            describe "visiting the user index" do
+              before { visit users_path }
+              it { should have_selector('title', text: 'Sign in') }
+            end
+          end
+
+          describe "visiting the edit page" do
+            before { visit edit_user_path(user) }
+            it { should have_selector('title', text: 'Sign in') }
+          end
+
+          describe "submitting to the update action" do
+            before { put user_path(user) }
+            specify { response.should redirect_to(signin_path) }
+          end
+
+          describe "visiting the following page" do
+            before { visit following_user_path(user) }
+            it { should have_selector('title', text: 'Sign in') }
+          end
+
+          describe "visiting the followers page" do
+            before { visit followers_user_path(user) }
             it { should have_selector('title', text: 'Sign in') }
           end
         end
-
-        describe "visiting the edit page" do
-          before { visit edit_user_path(user) }
-          it { should have_selector('title', text: 'Sign in') }
-        end
-
-        describe "submitting to the update action" do
-          before { put user_path(user) }
-          specify { response.should redirect_to(signin_path) }
-        end
+    end  
+          
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
+      
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: full_title('Edit user')) }
       end
-    end
-    
-        describe "as wrong user" do
+      
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end       
+         
+        describe "as non-admin user" do
           let(:user) { FactoryGirl.create(:user) }
-          let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-          before { sign_in user }
+          let(:non_admin) { FactoryGirl.create(:user) }
           
-          describe "visiting Users#edit page" do
-            before { visit edit_user_path(wrong_user) }
-            it { should_not have_selector('title', text: full_title('Edit user')) }
-          end
+          before { sign_in non_admin }
           
-          describe "submitting a PUT request to the Users#update action" do
-            before { put user_path(wrong_user) }
+          describe "submitting a DELETE request to the Users#destroy action" do
+            before { delete user_path(user) }
             specify { response.should redirect_to(root_path) }
           end
-        end   
+        end
+            
 
-end   
+      
+      
+    # removed end to put on line 128
+    
+       
+ # removed end to put on line 84
+end
